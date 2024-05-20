b16280bc788c29758b3618a4ca9d34fdbfe70482
Leonid-poc <legeorg2004@gmail.com>
2024-05-20 22:52:24 +0500

    adding new version of app for Mirlan

--- app.py
+++ app.py
@@ -1,27 +1,36 @@
-from flask import Flask, request, jsonify, render_template
-import psycopg2
-from main import add_user
-from config import host, user as db_user, password, db_name
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def serve_registration_page():
-    return render_template('index.html')
+@app.route("/")
+def mainPage():
+    return render_template("index.html")
 
-@app.route('/register_page')
-def register_registration():
-    return render_template('register1.html')
+@app.route("/signin")
+def signin():
+    return render_template("signin.html")
+
+@app.route('/signup')
+def signup():
+    return render_template("signup.html")
+
+# @app.route('/')
+# def serve_registration_page():
+#     return render_template('index.html')
+
+# @app.route('/register_page')
+# def register_registration():
+#     return render_template('register1.html')
 
 
-@app.route('/register', methods=['POST'])
-def register():
-    data = request.get_json()
-    username = data['username']
-    password = data['password']
-    print(f"Received username: {username}, password: {password}")
-    add_user(username, password)
-    return jsonify(success=True)
+# @app.route('/register', methods=['POST'])
+# def register():
+#     data = request.get_json()
+#     username = data['username']
+#     password = data['password']
+#     print(f"Received username: {username}, password: {password}")
+#     add_user(username, password)
+#     return jsonify(success=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- main.py
+++ main.py
@@ -1,40 +1,40 @@
 import psycopg2
 from config import host, user as db_user, password, db_name
 
-def add_user(login, pas):
-   
-    connection = None 
-
+def create_conn():
     try:
-        connection = psycopg2.connect(
-            host=host,
-            user=db_user,
-            password=password,
-            database=db_name
-        )
-        
-        cursor = connection.cursor()
-        
-        insert_query = """
-        INSERT INTO test (login, password)
-        VALUES (%s, %s)
-        RETURNING id;
-        """
-        
-        cursor.execute(insert_query, (login, pas))
-        
-        user = cursor.fetchone()[0]
-        
-        connection.commit()
-        
-        print("User successfully add")
-            
-    except Exception as _ex:
-        print("Error", _ex)
-    finally:
-        if connection:
-            connection.close()
-            print("Connection closed")
-            
+        return psycopg2.connect(host=host,user=db_user,password=password,database=db_name)
+    except:
+        raise Exception("Не удалось подключится к БД, проверьте настройки config.py")
 
+# def add_user(login, pas):
+#     try:
+#         connection = psycopg2.connect(
+#             host=host,
+#             user=db_user,
+#             password=password,
+#             database=db_name
+#         )
         
+#         cursor = connection.cursor()
+        
+#         insert_query = """
+#         INSERT INTO test (login, password)
+#         VALUES (%s, %s)
+#         RETURNING id;
+#         """
+        
+#         cursor.execute(insert_query, (login, pas))
+        
+#         user = cursor.fetchone()[0]
+        
+#         connection.commit()
+        
+#         print("User successfully add")
+            
+#     except Exception as _ex:
+#         print("Error", _ex)
+#     finally:
+#         if connection:
+#             connection.close()
+#             print("Connection closed")
